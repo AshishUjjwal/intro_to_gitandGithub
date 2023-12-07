375dae4b7832698f45eda0f917fbc87ceeb2e6ef
Ashish Ujjwal <ashishfrommohania@gamil.com>
2023-12-07 18:59:23 +0530

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,49 @@
+// This Program is done by Ashish Ujjwal
+#include<bits/stdc++.h>
+using namespace std;
+
+#define  fastio()        ios::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+#define all(v) (v).begin(), (v).end()
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#endif
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define pb push_back
+#define all(X) (X).begin(), (X).end()
+#define np cout << -1 << endl
+#define rn return
+#define vi vector<int>
+#define vvi vector<vi>
+typedef long long int ll;
+typedef long double ld;
+typedef unsigned long long ull;
+const int mod = pow(10,9)+7 ;
+const int MAX = pow(10,5)+1 ;
+const int dx[8] = { 1 , -1 , 0 , 0 , 1 , 1 , -1 , -1 } ;
+const int dy[8] = { 0 , 0 , 1 , -1 , 1 , -1 , 1 , -1 } ;
+
+void solve(){
+    cout<<"This is my first session in my life"<<endl;
+    cout<<"This is my first session in my life"<<endl;
+    cout<<"This is my first session in my life"<<endl;
+    cout<<"This is my first session in my life"<<endl;
+    cout<<"This is my first session in my life"<<endl;
+    cout<<"This is my first session in my life"<<endl;
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+fastio();
+
+    ll t; cin>>t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}
